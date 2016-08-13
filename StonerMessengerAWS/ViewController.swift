19509bbda6dc0c9a35904885c9832eccb859780d
roodoodey <mathieugrettir@hotmail.com>
2016-08-13 18:21:09 +0000

    Initial Commit

---
+++ StonerMessengerAWS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StonerMessengerAWS
+//
+//  Created by Mathieu Skulason on 13/08/16.
+//  Copyright © 2016 Konta ehf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
